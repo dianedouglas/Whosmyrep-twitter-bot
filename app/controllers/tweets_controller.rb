59cf4cc55ff81d6849403e65b955c00183e00001
lee kebler & Diane Douglas <student@epicodus.com>
2014-09-18 17:47:00 -0700

    WIP

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,26 +1,9 @@
 class TweetsController < ApplicationController
 
+
   def index
-    client = Twitter::REST::Client.new do |config|
-    config.consumer_key = 'LDNk55uXbOoG1f4ZCvFspZW9l'
-    config.consumer_secret = 'xlpFEIl8SkFgViLDwQe2b3yJ7clLCBsgNaxDOp1Q7wmpQuknqC'
-    config.access_token = '150020012-5V4DUTwu9xJ6aF3ZY6mK0MdHLVMNKnSuXlRVA1IA'
-    config.access_token_secret = 'w0x56NiADq4fTuVtG8nsFYKL1BIwxUtx8x0ce9zO4KG9L'
-    end
-    @tweets = []
-    mentions = client.mentions_timeline
-    mentions.each do |tweet|
-      user = '@' + tweet.user.username
-      client.update("Hello " + user + "!")
-      @tweets << user
-    end
-    # binding.pry
-    # topics = ["whosmyrep"]
-
-
-    # @mentions = client.filter(:track => client) do |object|
-    #   puts object.text if object.is_a?(Twitter::Tweet)
-    # end
+    @tweet = Tweet.new
+    @tweet.check_mentions
   end
 end
 
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,30 @@
 class Tweet < ActiveRecord::Base
 
-  def hash_search
+  def check_mentions
+      @senators = { "OR" => "Oregon Information", "CA" => "California Information"}
 
 
+      client = Twitter::REST::Client.new do |config|
+      config.consumer_key = 'LDNk55uXbOoG1f4ZCvFspZW9l'
+      config.consumer_secret = 'xlpFEIl8SkFgViLDwQe2b3yJ7clLCBsgNaxDOp1Q7wmpQuknqC'
+      config.access_token = '150020012-5V4DUTwu9xJ6aF3ZY6mK0MdHLVMNKnSuXlRVA1IA'
+      config.access_token_secret = 'w0x56NiADq4fTuVtG8nsFYKL1BIwxUtx8x0ce9zO4KG9L'
+      end
+
+      mentions = client.mentions_timeline
+
+      mentions.each do |mention|
+        if mention.hashtags != []
+          mention.hashtags.each do |hash|
+            if @senators.include?(hash.attrs[:text])
+              begin
+                client.update("Hey @" + mention.user.screen_name + " " + @senators[hash.attrs[:text]], :in_reply_to_status_id => mention.id)
+              rescue
+                client.update("Hey @" + mention.user.screen_name + " " + @senators[hash.attrs[:text]] + Time.now.to_s, :in_reply_to_status_id => mention.id)
+              end
+            end
+          end
+        end
+      end
   end
-
 end
